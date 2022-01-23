8244214e87211a50694d64f676eac384004b3f0a
Christian Castillo <castillochristian3@gmail.com>
2022-01-23 18:03:55 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.getElementById("main").remove();
+
+document.createElement("h1");
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Christian is the champion!";
+
+document.body.append(newHeader);
